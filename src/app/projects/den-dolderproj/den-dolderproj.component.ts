cec554ca7de5a2d53301fd783835711e23506cd5
Giuseppe Zamboni <beppujah@gmail.com>
2017-07-23 23:24:14 +0200

    ci sto prendendo gusto

--- src/app/projects/den-dolderproj/den-dolderproj.component.ts
+++ src/app/projects/den-dolderproj/den-dolderproj.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DenDolderprojComponent implements OnInit {
 
+	toggleModal: boolean=false;
+
+toggleModal2: boolean=false;
+
+
   constructor() { }
 
   ngOnInit() {
